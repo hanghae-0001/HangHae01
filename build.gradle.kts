60797bde413bb6e26cdb49ef6edb9b1657572861
jimmy.lee <dongeol92@daum.net>
2023-09-18 15:23:07 +0900

    chore: db connection setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("io.github.microutils:kotlin-logging-jvm:2.0.10")
     compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
     runtimeOnly("com.mysql:mysql-connector-j")
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
