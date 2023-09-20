60e82a6b71528476fc3a2e86ad43c74b03214482
jimmy.lee <dongeol92@daum.net>
2023-09-20 09:08:44 +0900

    test: 상점 생성 tc 추가

---
+++ src/test/kotlin/com/hanghae/health/store/application/StoreCreateServiceTest.kt
@@ -0,0 +1,17 @@
+package com.hanghae.health.store.application
+
+import com.hanghae.health.testconfiguration.UnitTest
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+
+@UnitTest
+@DisplayName("Given: create()")
+class StoreCreateServiceTest {
+
+    @Nested
+    @DisplayName("When: 상점 등록 요청 시, ")
+    internal inner class when_store_create_request_is_received {
+
+        // TODO : 요청 값 검증 필요. 요청 값 정의 후 TC 작성
+    }
+}
