d8625e45034e19ef2e66e54c1f579dd7a6c630c8
KATEKEITH <46472768+KATEKEITH@users.noreply.github.com>
2023-09-22 09:33:35 +0900

    #26 test 결제

---
+++ src/test/kotlin/com/hanghae/health/payment/CreatePaymentTest.kt
@@ -0,0 +1,17 @@
+package com.hanghae.health.payment
+
+import org.junit.jupiter.api.Test
+
+class CreatePaymentTest {
+
+    // 외부 인터페이스에 해당 하는 결제 수단 (PG사, 포인트, 쿠폰) 시스템은 Mock 처리
+
+    @Test
+    fun `결제가 실패하면 PayFailException이 발생한다`() {
+    }
+
+    @Test
+    fun `결제 성공 후 주문은 발송 대기 상태로 변경된다`() {
+    }
+
+}
