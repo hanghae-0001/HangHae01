ade090edefacc9302e5f57028e9eba57f322df7a
Sangmin <sm9171@nate.com>
2023-10-01 16:51:27 +0900

    feat(User): user validate 체크 - name, email, address 필드 유무 체크 - user 객체 단위 테스트 작성

--- commerce-api/src/main/kotlin/com/hanghae/commerce/user/domain/User.kt
+++ commerce-api/src/main/kotlin/com/hanghae/commerce/user/domain/User.kt
@@ -7,6 +7,18 @@ class User(
     val address: String,
     var id: Long? = null,
 ){
+    init {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("이름은 비어 있을 수 없습니다")
+        }
+        if (email.isBlank()) {
+            throw IllegalArgumentException("이메일은 비어 있을 수 없습니다")
+        }
+        if (address.isBlank()) {
+            throw IllegalArgumentException("주소는 비어 있을 수 없습니다")
+        }
+    }
+
     companion object {
         fun createUser(name: String, age: Int, email: String, address: String): User {
             return User(name, age, email, address)
