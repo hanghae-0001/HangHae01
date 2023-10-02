96d321d67d85faf6b6d4e74ab05fc619544e6162
Sangmin <sm9171@nate.com>
2023-10-02 23:07:09 +0900

    feat(user): transactional 추가

--- commerce-infra/db-main/src/main/kotlin/com/hanghae/commerce/data/domain/UserEntityRepository.kt
+++ commerce-infra/db-main/src/main/kotlin/com/hanghae/commerce/data/domain/UserEntityRepository.kt
@@ -4,18 +4,21 @@ import com.hanghae.commerce.user.domain.User
 import com.hanghae.commerce.user.domain.UserRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 class UserEntityRepository(
     private val jpaUserRepository: JpaUserRepository,
 ) : UserRepository {
 
+    @Transactional
     override fun save(user: User): Long {
         return jpaUserRepository.save(
             UserEntity.toEntity(user),
         ).id!!
     }
 
+    @Transactional(readOnly = true)
     override fun read(id: Long): User? {
         return jpaUserRepository.findByIdOrNull(id)?.let {
             User(
