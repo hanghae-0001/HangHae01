92f3434bc9fc3c01731b6d94f4a8b940e4478705
jimmy.lee <dongeol92@daum.net>
2023-10-04 22:43:17 +0900

    test: order tc 구현 > when_stock_size_is_5

--- commerce-api/src/test/kotlin/com/hanghae/commerce/order/application/OrderCreateServiceTest.kt
+++ commerce-api/src/test/kotlin/com/hanghae/commerce/order/application/OrderCreateServiceTest.kt
@@ -1,34 +1,88 @@
 package com.hanghae.commerce.order.application
 
+import com.hanghae.commerce.item.application.ItemWriter
+import com.hanghae.commerce.item.domain.Item
+import com.hanghae.commerce.order.domain.OrderCreateService
+import com.hanghae.commerce.order.domain.OrderItem
+import com.hanghae.commerce.order.domain.command.OrderCreateCommand
+import com.hanghae.commerce.order.exception.SoldOutException
 import com.hanghae.commerce.testconfiguration.EnableTestcontainers
 import com.hanghae.commerce.testconfiguration.IntegrationTest
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Nested
-import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.*
 import org.springframework.beans.factory.annotation.Autowired
+import java.util.*
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
 
 @IntegrationTest
 @EnableTestcontainers
 @DisplayName("Given: orderCreate()")
-class OrderCreateServiceTest(
+class OrderCreateServiceIT(
+    @Autowired
+    var itemWriter: ItemWriter,
 //    @Autowired
-//    var productRepository: ProductRepository,
-//    @Autowired
-//    var productStockCountService: ProductStockCountService,
-//    @Autowired
-//    var sut: OrderCreateService,
+//    var itemStockCountService: ItemStockCountService,
+    @Autowired
+    var sut: OrderCreateService,
 ) {
+
     @Nested
     @DisplayName("When: 25,000원짜리 상품 재고가 5개 일 때,")
     internal inner class when_stock_size_is_5 {
+
+        lateinit var item: Item
+
+        @BeforeEach
+        fun setUp() {
+            item = Item.of(
+                id = "1",
+                name = "Item Fixture",
+                price = 25000,
+                stock = 5L,
+            )
+            itemWriter.write(item)
+        }
+
+        @AfterEach
+        fun tearDown() {
+            itemWriter.deleteAll()
+        }
+
         @Test
         @DisplayName("Then: 6개 주문하면, OutOfStockException 발생한다.")
         fun tc1() {
+            // when
+            assertThatThrownBy {
+                sut.create(
+                    orderCreateCommandFixture(
+                        itemId = item.id,
+                        price = 20_000,
+                        quantityPerRequest = 6,
+                    ),
+                )
+
+            // then
+            }.isInstanceOf(SoldOutException::class.java)
+                .hasMessage("주문한 상품량이 재고량보다 큽니다.")
         }
 
         @Test
         @DisplayName("Then: 존재하지 않는 상품번호를 요청하면, IllegalArgumentException 발생한다.")
         fun tc2() {
+            // when
+            assertThatThrownBy {
+                sut.create(
+                    orderCreateCommandFixture(
+                        itemId = "NOT_EXIST_ITEM_ID",
+                        price = 20_000,
+                        quantityPerRequest = 7,
+                    ),
+                )
+
+            // then
+            }.isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("존재하지 않는 상품입니다.")
         }
     }
 
@@ -50,6 +104,7 @@ class OrderCreateServiceTest(
         @DisplayName("Then: 요청 300개가 동시에 4개씩 주문하면, 요청 50개는 OutOfStockException 발생한다.")
         fun tc3() {
         }
+
     }
 
     @Nested
@@ -61,7 +116,7 @@ class OrderCreateServiceTest(
         }
 
         @Test
-        @DisplayName("Then: 미리 등록된 주소가 없다면 NoAddressToDeleveryException 발생한다.")
+        @DisplayName("Then: 미리 등록된 주소가 없다면 NoAddressToDeliveryException 발생한다.")
         fun tc2() {
         }
     }
@@ -88,22 +143,24 @@ class OrderCreateServiceTest(
         fun tc2() {
         }
     }
+
+    private fun orderCreateCommandFixture(
+        itemId: String,
+        price: Int,
+        quantityPerRequest: Int,
+    ): OrderCreateCommand {
+        return OrderCreateCommand(
+            listOf(
+                OrderItem(
+                    itemId,
+                    price,
+                    quantityPerRequest,
+                ),
+            ),
+        )
+    }
+
+    private fun orderCreateCommandFixture(vararg orderItems: OrderItem): OrderCreateCommand {
+        return OrderCreateCommand(orderItems.toList())
+    }
 }
-/*
-0. 아이스브레이킹
-    - 다들 구현하고 있는 지 ?
-1. 레디스 연동
-    - 캐시
-    - 분산락 - 사례 질문
-    - 메시지 브로커 용도
-2. 테스트 컨테이너
-    -
-    - 동시성 테스트 구현 용도
-3. 각자 도메인 구현하고 있는데, 나중에 따로 병합하기 보다 작업하는 대로 확인할 수 있도록 하기 위한 방법 고민 필요
-4. 공통 응답 정의 필요
-    - 데이터
-    - 에러
-5. 도메인 계층에는 어떤 클래스들을 만드는 지?
-    -
-6.
- */
