1fa819f50585ebde4b88103763ea629d61864b67
jimmy.lee <dongeol92@daum.net>
2023-10-07 16:57:35 +0900

    feat: item persistence 레이어 추가

---
+++ commerce-infra/db-main/src/main/kotlin/com/hanghae/commerce/data/domain/item/ItemMapper.kt
@@ -0,0 +1,14 @@
+package com.hanghae.commerce.data.domain.item
+
+import com.hanghae.commerce.item.domain.Item
+
+fun Item.toEntity(): ItemEntity {
+    TODO("Not yet implemented")
+}
+
+fun ItemEntity.toDomain(): Item {
+    TODO("Not yet implemented")
+}
+
+
+
