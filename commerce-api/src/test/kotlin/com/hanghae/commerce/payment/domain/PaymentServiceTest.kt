4dad105cae8200d074377010439a32546ff1ee85
jimmy.lee <dongeol92@daum.net>
2023-10-10 00:11:28 +0900

    test: payment tc 추가

---
+++ commerce-api/src/test/kotlin/com/hanghae/commerce/payment/domain/PaymentServiceTest.kt
@@ -0,0 +1,29 @@
+package com.hanghae.commerce.payment.domain
+
+import com.hanghae.commerce.testconfiguration.UnitTest
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@UnitTest
+@DisplayName("Given: payment()")
+class PaymentServiceTest {
+
+    @Nested
+    @DisplayName("When: 결제가 완료되면, ")
+    internal inner class when_payment_complete {
+        @Test
+        @DisplayName("Then: 주문상태는 '결제대기' -> '발송대기' 상태로 변경된다.")
+        fun tc1() {
+        }
+    }
+
+    @Nested
+    @DisplayName("When: 분당 100건 이상 요청이 발생하면, ")
+    internal inner class when_more_than_300_requests_occur_per_minute {
+        @Test
+        @DisplayName("Then: Circuit Breaker가 발동한다.")
+        fun tc1() {
+        }
+    }
+}
