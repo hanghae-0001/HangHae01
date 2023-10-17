8bddaf13156429a28c988280d5dec8bdcfa28544
jimmy.lee <dongeol92@daum.net>
2023-10-17 18:12:58 +0900

    chore: logback 설정

--- commerce-support/logging/build.gradle.kts
+++ commerce-support/logging/build.gradle.kts
@@ -1,3 +1,5 @@
 dependencies {
     api("io.github.microutils:kotlin-logging-jvm:2.0.10")
+    implementation(group = "ca.pjer", name = "logback-awslogs-appender", version = "1.6.0") // 로그백 의존성
+    implementation("org.codehaus.janino:janino:3.1.7") // 프로퍼티 제어 in xml
 }
