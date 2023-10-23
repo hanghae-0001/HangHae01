6b0d15a3ca9bf817b18e8836df876dc7f77387c4
jimmy.lee <dongeol92@daum.net>
2023-10-24 08:49:45 +0900

    test: claim > add OrderCancelServiceTest tc

---
+++ commerce-api/src/test/kotlin/com/hanghae/commerce/claim/domain/OrderCancelValidatorTest.kt
@@ -0,0 +1,47 @@
+package com.hanghae.commerce.claim.domain
+
+import com.hanghae.commerce.testconfiguration.UnitTest
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@UnitTest
+@DisplayName("Given: validate()")
+class OrderCancelValidatorTest {
+
+    @Nested
+    @DisplayName("When: ")
+    inner class Context1 {
+
+        @Test
+        @DisplayName("Then: ")
+        fun tc1() {
+
+        }
+
+        @Test
+        @DisplayName("Then: ")
+        fun tc2() {
+
+        }
+
+    }
+
+    @Nested
+    @DisplayName("When: ")
+    inner class Context2 {
+
+        @Test
+        @DisplayName("Then: ")
+        fun tc1() {
+
+        }
+
+        @Test
+        @DisplayName("Then: ")
+        fun tc2() {
+
+        }
+
+    }
+}
